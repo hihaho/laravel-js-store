ba1e64b0e34a2ac2ea10623435ad4a00d980b9c6
Sander Muller <sander@mulver.nl>
2024-01-08 16:55:02 +0100

    Render store data in the DOM using the @json blade directive

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -4,7 +4,7 @@
 
 <script
     id="laravel-js-store-{{ $rand }}"
-    data-store="{{ json_encode(frontend_store()->data()) }}"
+    data-store='@json(frontend_store()->data(), JSON_THROW_ON_ERROR | JSON_HEX_APOS | JSON_HEX_QUOT)'
 >
     var el = document.getElementById('laravel-js-store-{{ $rand }}')
     window.{{ config('js-store.window-element') }} = JSON.parse(el.dataset.store)
